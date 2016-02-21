bf36a120d896c811f17eafdaf00a594642661cca
tharindarodrigo <tharindarodrigo@gmail.com>
2016-02-21 20:17:20 +0530

    react skeleton

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+//console.log("hello");
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+//console.log("hello");
+
+var List = require('./components/List.jsx');
+
+
+ReactDOM.render(<List />, document.getElementById('ingredients'));
